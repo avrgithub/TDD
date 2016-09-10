6a41281ea5f5a63ae6c84962c665ef1c2edd1857
avrgithub <addanki.mahalakshmi@gmail.com>
2016-09-09 18:52:16 -0700

    Update ConvertRomanNumbers.cs
    tested version

--- ConvertRomanNumbers.cs
+++ ConvertRomanNumbers.cs
@@ -8,45 +8,50 @@ namespace ConsoleApplication1
 {
     class Program
     {
-      
+
         static void Main(string[] args)
         {
-          string romanNum = "MMXVI";
-          int result = RomanToInteger(roamNum);
-          
-          Console.WriteLine("Roman: %s  Integer:%d", romanNum, result);
+            string romanNum = "MMXVI";
+            int result = RomanToInteger(romanNum);
+
+            Console.WriteLine("Roman:{0}   Integer:{1}", romanNum, result);
+
+            Console.ReadKey();
+
             return;
         }
-    
-
-private static Dictionary<char, int> RomanIntMap = new Dictionary<char, int>()
-    {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
 
 
-public static int RomanToInteger(string roman)
-{
-    int number = 0;
-    for (int i = 0; i < roman.Length; i++)
-    {
-        if (i + 1 < roman.Length && RomanMap[roman[i]] < RomanMap[roman[i + 1]])
+
+        private static Dictionary<char, int> RomanIntMap = new Dictionary<char, int>()
         {
-            number -= RomanMap[roman[i]];
-        }
-        else
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+
+        public static int RomanToInteger(string roman)
         {
-            number += RomanMap[roman[i]];
+            int number = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && RomanIntMap[roman[i]] < RomanIntMap[roman[i + 1]])
+                {
+                    number -= RomanIntMap[roman[i]];
+                }
+                else
+                {
+                    number += RomanIntMap[roman[i]];
+                }
+            }
+            return number;
         }
+
+
     }
-    return number;
-}
-
-}
 }
